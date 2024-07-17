cd2443947c9b78586ccf78c0d604187fd5064f03
Sreerag Rajesh Babu <Sreerag.Babu@asl.mum.sst>
2024-07-17 19:39:20 +0530

    innit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using InheritenceDemo.Models;
+
+namespace InheritenceDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee();
+            Console.WriteLine("==============");
+            Manager m1 = new Manager();
+            
+            Employee m2 = new Employee(54);
+            Console.WriteLine("==============");
+            Manager m3 = new Manager(45);
+
+        }
+    }
+}
